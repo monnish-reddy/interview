17f419f091f0e026970b66e766ee29c836a83448
monnish-reddy <peta.monnish@gmail.com>
2025-07-01 22:31:40 -0400

    latest

--- disk_utilization_alert.sh
+++ disk_utilization_alert.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "monnish"
+USERID=$(id -u)
+SCRIPT_NAME= $(echo $0|cut -d '.' -f1)
+LOGS_FOLDER= "/var/log/monnishlogs"
+LOG_FILE= "$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+echo " $LOG_FILE"
